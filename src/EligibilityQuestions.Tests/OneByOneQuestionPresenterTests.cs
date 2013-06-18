87a600114f1c7a8f258846c12bcce259f8fd7af0
Bob Pace <bob.pace@gmail.com>
2013-06-18 00:36:03 -0600

    spike of how question might work

---
+++ src/EligibilityQuestions.Tests/OneByOneQuestionPresenterTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using FubuTestingSupport;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace EligibilityQuestions.Tests
+{
+    public class OneByOneQuestionPresenterTests : InteractionContext<OneByOneQuestionPresenter>
+    {
+        protected override void beforeEach()
+        {
+        }
+
+        [Test]
+        public void questions_can_target_properties_on_result_model()
+        {
+            var first = new Question<EndResultModel>(x => x.LikesBlue);
+            first.Answer = true;
+            var second = new Question<EndResultModel>(x => x.LikesGreen);
+            second.Answer = true;
+            var third = new Question<EndResultModel>(x => x.Birthday);
+            var today = DateTime.Today;
+            third.Answer = today;
+
+            var questions = new[] {first, second, third};
+            var result = new EndResultModel();
+            questions.Each(x => x.SetAnswer(result));
+
+            result.LikesBlue.ShouldBeTrue();
+            result.LikesGreen.Value.ShouldBeTrue();
+            result.Birthday.ShouldEqual(today);
+        }
+
+        [Test]
+        public void may_have_more_than_one_nested_question()
+        {
+        }
+
+        [Test]
+        public void future_questions_are_hidden_until_after_you_answer_the_current_one()
+        {
+        }
+
+        [Test]
+        public void previously_answered_questions_are_shown()
+        {
+        }
+
+        [Test]
+        public void you_can_answer_the_current_question_and_get_the_next_one()
+        {
+        }
+
+        [Test]
+        public void eventually_you_run_out_of_questions_and_trigger_a_calculated_result()
+        {
+        }
+
+    }
+}
---
+++ src/EligibilityQuestions/Question.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FubuCore;
+using FubuCore.Reflection;
+
+namespace EligibilityQuestions
+{
+
+    public class EndResultModel
+    {
+        public bool LikesBlue { get; set; }
+        public bool? LikesGreen { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    public interface IQuestion
+    {
+        Accessor Accessor { get; set; }
+        string QuestionText { get; set; }
+        string HelpText { get; set; }
+    }
+
+    public class Question<TResult> : IQuestion
+    {
+        private object _answer;
+
+        public Question(Expression<Func<TResult,object>> accessor)
+        {
+            Accessor = accessor.ToAccessor();
+        }
+
+        public Accessor Accessor { get; set; }
+        public string QuestionText { get; set; }
+        public string HelpText { get; set; }
+
+        public object Answer
+        {
+            get { return _answer; }
+            set
+            {
+                ValidateSetWillSucceed(value.GetType());
+                _answer = value;
+            }
+        }
+
+        private void ValidateSetWillSucceed(Type valueType)
+        {
+            Action<Type> errorIfIncompatible = type =>
+            {
+                if (type != valueType)
+                {
+                    throw new ArgumentException("wrong property type for answer");
+                }
+            };
+
+            errorIfIncompatible(Accessor.PropertyType.IsNullable()
+                ? Accessor.PropertyType.GetGenericArguments().First()
+                : Accessor.PropertyType);
+        }
+
+        public void SetAnswer(TResult instance)
+        {
+            Accessor.SetValue(instance, Answer);
+        }
+    }
+
+    public interface IQuestionProcessor<TResult>
+    {
+        IEnumerable<IQuestion> Questions { get; set; }
+        void Process();
+    }
+
+
+    public class DefaultQuestionProcessor<TResult> : IQuestionProcessor<TResult>
+    {
+        public DefaultQuestionProcessor(IEnumerable<IQuestion> questions)
+        {
+            Questions = questions;
+        }
+
+        public IEnumerable<IQuestion> Questions { get; set; }
+
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class OneByOneQuestionPresenter : IQuestionPresenter
+    {
+        private readonly IQuestionProcessor<EndResultModel> _questionProcessor;
+
+        public OneByOneQuestionPresenter(IQuestionProcessor<EndResultModel> questionProcessor)
+        {
+            _questionProcessor = questionProcessor;
+        }
+    }
+
+    public interface IQuestionPresenter
+    {
+    }
+
+}
